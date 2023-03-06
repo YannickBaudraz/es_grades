1c95c059ca6090f0733cd6ec35ccba9a5410ba59
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-03-06 17:05:22 +0100

    Add type in people table for STI

--- db/migrate/20230306143300_create_people.rb
+++ db/migrate/20230306143300_create_people.rb
@@ -1,6 +1,7 @@
 class CreatePeople < ActiveRecord::Migration[7.0]
   def change
     create_table :people do |t|
+      t.string :type, null: false
       t.string :first_name
       t.string :last_name
       t.string :email
