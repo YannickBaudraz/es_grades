861f0e9bb2b82d66786f4334f3ffd04edaf4350a
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-03-31 20:21:31 +0200

    Add courses management

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -50,13 +50,14 @@ class CoursesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def course_params
-      params.fetch(:course, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def course_params
+    params.require(:course).permit(:slug, :title, :description, :category_id, :quarter_id, :promotion_id)
+  end
 end
--- db/migrate/20230313142425_create_quarters.rb
+++ db/migrate/20230313142425_create_quarters.rb
@@ -1,7 +1,7 @@
 class CreateQuarters < ActiveRecord::Migration[7.0]
   def change
     create_table :quarters do |t|
-      t.datetime :started_at
+      t.date :started_at
       t.date :ended_at
 
       t.references :semester, null: false, foreign_key: true
